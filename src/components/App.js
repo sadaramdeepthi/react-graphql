ab3d6cbe813cef0c5563bbd7b9ec6de62d002503
sadaramdeepthi <sadaramdeepthi@gmail.com>
2021-01-18 13:42:10 +0530

    implementing graphQl with react

---
+++ src/components/App.js
@@ -0,0 +1,56 @@
+import React, { Component } from "react";
+import TopLanguages from "./TopLanguages";
+import gql from "graphql-tag";
+import { Query } from "react-apollo";
+
+const REPOSITORIES = gql`
+  {
+    viewer {
+      repositories(last: 100, isFork: false) {
+        nodes {
+          name
+          description
+          url
+          languages(first: 5) {
+            nodes {
+              color
+              name
+            }
+          }
+        }
+      }
+    }
+  }
+`;
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <div
+          style={{
+            width: "100%",
+            height: "75vh",
+            maxHeight: "600px",
+            maxWidth: "600px",
+            margin: "0 auto",
+            fontFamily:
+              '-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"',
+          }}
+        >
+          <h2 style={{ textAlign: "center" }}>Top Languages</h2>
+
+          <Query query={REPOSITORIES} variables={{}}>
+            {({ data, loading }) =>
+              loading ? (
+                <span>loading data...</span>
+              ) : (
+                <TopLanguages repositories={data.viewer.repositories} />
+              )
+            }
+          </Query>
+        </div>
+      </div>
+    );
+  }
+}
